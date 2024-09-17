4f5e52cf7905f7c305bd2f469deaf3879286b9b6
Gangulr <gangulr30@gmail.com>
2024-09-17 19:25:30 +0530

    gangul commit 17

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LeaveRequestForm from './leaveReqForm/LeaveForm';
-import SalaryMang from './SalaryMangment/SalaryMang';
-import AttendanceTrackingForm from './AttendanceT/Attencance';
-import HRManagementInterface from './HRManagementInterface/HRInterface';
-import SalaryRet from './SalaryDetailsRet/SalaryRet';
-import HRManagementReport from './HRreport/HRreport';
-import ARet from './ATrackingRet/ARet';
-import LeaveRet from './LeaveRetrive/LeaveRet';
-// import Addemp from './AddEmp/Addemp';
-// import EmpRet from './AddEmpRet/empRet';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+i//mport LeaveRequestForm from './leaveReqForm/LeaveForm';
+//import SalaryMang from './SalaryMangment/SalaryMang';
+//import AttendanceTrackingForm from './AttendanceT/Attencance';
+//import HRManagementInterface from './HRManagementInterface/HRInterface';
+//import SalaryRet from './SalaryDetailsRet/SalaryRet';
+//import HRManagementReport from './HRreport/HRreport';
+//import ARet from './ATrackingRet/ARet';
+//import LeaveRet from './LeaveRetrive/LeaveRet';
+//import Addemp from './AddEmp/Addemp';
+import EmpRet from './AddEmpRet/empRet';
+ //Added import for EmpRet
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        {/* Render standalone components here if needed */}
-        <SalaryRet />
-        <ARet />
-        <LeaveRet />
-        {/* <Addemp /> */}
-        {/* <EmpRet /> */}
+  <div>
+        <EmpRet/> 
+      
+  </div>
+    
 
-        <Routes>
-          <Route path="/" element={<HRManagementInterface />} />
-          <Route path="/leaveform" element={<LeaveRequestForm />} />
-          <Route path="/salarymang" element={<SalaryMang />} />
-          <Route path="/attendancetrack" element={<AttendanceTrackingForm />} />
-          <Route path="/hr-report" element={<HRManagementReport />} />
-        </Routes>
-      </div>
-    </Router>
   );
 };
 
 export default App;
+
